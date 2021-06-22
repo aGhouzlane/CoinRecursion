285fbe9a2fd034a81a2db6c805f221eb1804dcb4
aGhouzlane <ahmedghouzlane@gmail.com>
2021-06-22 18:25:06 -0400

    Add project files

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace recursion
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double[] testArray = GetCoins(4.99, 0);
+
+      for (int i = 0; i < testArray.Length; i++)
+      {
+        Console.WriteLine(testArray[i]);
+      }
+    }
+    
+    static double[] resultArray = { 0, 0, 0, 0 };
+    public static double[] GetCoins(double n, int i)
+    {
+      double[] coins = { .25, .10, .05, .01 };
+      resultArray[i] = Math.Floor(n / coins[i]);
+      //base case
+      if (i >= 3)
+      {
+        return resultArray;
+      }
+      return GetCoins(n % coins[i], i + 1);
+    }
+  }
+}
